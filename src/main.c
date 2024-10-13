fbb452ddc4600f98055fe17b06acb75fdbc99a50
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2024-10-13 23:44:16 +0200

    change the main a little bit notably adding the shell for me to remeber

--- src/main.c
+++ src/main.c
@@ -5,17 +5,21 @@
 ** main
 */
 
-#include "itlei.h"
-#include <string.h>
 #include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include "itlei.h"
 
-const char default_program[] = "let a = 1;\nprint(a)";
+const char default_program[] = "let a = 1;\n"
+                                "print(a)";
 
 int main(int argc, char *argv[argc])
 {
     errno = 0;
-    if (argc == 1)
+    if (argc == 1) {
         ITLEI_RUNSTR(default_program);
+        shell_run();
+    }
     else
         interpretor_run_from_file(argv[1]);
     return errno;
