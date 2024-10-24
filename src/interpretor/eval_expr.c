e821d62144ecf395941a2cb5bcb7bfe5e89e1666
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2024-10-24 23:24:55 +0200

    add placeholder for treewalk interpreter

---
+++ src/interpretor/eval_expr.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** /home/vj/coding/itlei/src/interpretor/eval_expr
+** File description:
+** expression evaluator
+*/
+
+#include "general/macros.h"
+
+int interpretor_eval_expr(char *line)
+{
+    (void)line;
+    TODO;
+
+    return 0;
+}
