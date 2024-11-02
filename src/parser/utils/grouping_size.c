1bbcc683b73b79e48f6532ed0154f74a04eff13f
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2024-11-02 01:37:12 +0100

    utility for grouping size

---
+++ src/parser/utils/grouping_size.c
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2024
+** /home/vj/coding/itlei/src/parser/grouping_size
+** File description:
+** utilities for gourping size
+*/
+
+#include "lexer/type.h"
+#include "parser/macros.h"
+#include "parser/type.h"
+
+size_t parser_grouping_size(parser_t *parser)
+{
+    size_t initial_index = parser->lexem_index;
+    size_t indent = 1;
+
+    if (CUR_LEXEM(parser).type != lx_par_open)
+        TODO;
+    for (parser->lexem_index++; indent; parser->lexem_index++) {
+        switch (CUR_LEXEM(parser).type) {
+            case lx_par_open:
+                indent++;
+                continue;
+            case lx_par_close:
+                indent--;
+            default:
+                continue;
+        }
+    }
+    indent = parser->lexem_index - initial_index;
+    parser->lexem_index = initial_index;
+    return indent;
+}
