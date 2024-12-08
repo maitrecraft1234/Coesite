26b502829b9ab84e65ce98e9030fc9e1bb184ee9
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2024-12-08 23:19:43 +0100

    empty files for decl and expresstioph
    I need to get this done

---
+++ src/parser/meth/decl.c
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2024
+** src/parser/meth/decl
+** File description:
+** declaration for things and whatnot this is stupid to put in a separate file
+*/
+
+
---
+++ src/parser/meth/expression/expression.c
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2024
+** /home/vj/coding/itlei/src/parser/meth/expression/expression
+** File description:
+** entry point for the expreson parser
+*/
+
+
