0c9689482685872945f98c1352258eec1ffa88c2
Victor Jurgens Mestre <victor.jurgens-mestre@epitech.eu>
2024-12-16 14:43:51 +0100

    basic structure for a file nothing really happening here

--- src/parser/meth/expression/expression.c
+++ src/parser/meth/expression/expression.c
@@ -5,4 +5,22 @@
 ** entry point for the expreson parser
 */
 
+#include <parser/type.h>
+#include "parser/grammar_types/meth/expression.h"
+#include "general/dynamic_array.h"
 
+static pgmx_additive_t pgm_expr_additive(parser_t *parser)
+{
+    pgmx_additive_t additive = {.ops = da_create(), .left = da_create()};
+
+    //while loop to parse the expression
+    return additive;
+}
+
+pgm_expression_t pgm_expression(parser_t *parser)
+{
+    pgm_expression_t expression = {0};
+
+    expression = pgm_expr_additive(parser);
+    return expression;
+}
