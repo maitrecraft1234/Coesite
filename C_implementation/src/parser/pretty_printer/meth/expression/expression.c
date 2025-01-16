4f668817e71e51ebf465d8bd5aa8832cfc8d68a0
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2025-01-16 17:31:54 +0100

    first basic iteration of parser complete with extremely barebones pretty printer
    most of the work here was done to lay down the structure for the rest of
    the api, function parsing will problably be the next step (that of
    fixing the pretty printer for anything that isn't a int litteral) but
    after that error handling and we can use this shit in prod

---
+++ C_implementation/src/parser/pretty_printer/meth/expression/expression.c
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2025
+** src/parser/pretty_printer/meth/expression
+** File description:
+** pretty printer for expression
+*/
+
+#include "parser/grammar_types/meth/expression.h"
+#include <stdio.h>
+#include "general/macros.h"
+#include "lexer/functions.h"
+#include "parser/print/functions.h"
+#include "general/dynamic_array.h"
+#include "parser/grammar_types/meth/type_tag.h"
+
+static void parser_dump_meth_primary(pgmx_primary_t *primary)
+{
+    switch (primary->type) {
+        case PGM_BLOCK:
+        case PGM_GROUPING:
+        case PGM_UNARY:
+        case PGM_LITERAL:
+            printf(" %ld ", primary->literal.literal.value);
+            break;
+
+        default:
+            UNREACHABLE;
+    }
+}
+
+static void parser_dump_meth_expr_mul(pgmx_multiplicative_t *expr)
+{
+    printf("(");
+    parser_dump_meth_primary(&expr->left);
+    for (size_t i = 0; i < DA_LEN(expr->ops); ++i) {
+        lexem_dbg_id_print(expr->ops[i].operator);
+        parser_dump_meth_primary(&expr->ops[i].right);
+    }
+    printf(")");
+}
+
+void parser_dump_meth_expression(pgm_expression_t *expr)
+{
+    printf("(");
+    parser_dump_meth_expr_mul(&expr->left);
+    for (size_t i = 0; i < DA_LEN(expr->ops); ++i) {
+        lexem_dbg_id_print(expr->ops[i].operator);
+        parser_dump_meth_expr_mul(&expr->ops[i].right);
+    }
+    printf(")");
+}
