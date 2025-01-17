157bbd857a9fc3867db0e317f216fc6d755812d9
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2025-01-17 16:08:31 +0100

    add better expression printing

--- C_implementation/src/parser/pretty_printer/meth/expression/expression.c
+++ C_implementation/src/parser/pretty_printer/meth/expression/expression.c
@@ -17,12 +17,14 @@ static void parser_dump_meth_primary(pgmx_primary_t *primary)
 {
     switch (primary->type) {
         case PGM_BLOCK:
+            return parser_dump_meth_block(primary->block);
         case PGM_GROUPING:
+            return parser_dump_meth_expression(primary->grouping.expr);
         case PGM_UNARY:
+            lexem_dbg_id_print(primary->unary.operator);
+            return parser_dump_meth_expression(primary->unary.expr);
         case PGM_LITERAL:
-            printf(" %ld ", primary->literal.literal.value);
-            break;
-
+            return (void)printf(" %ld ", primary->literal.literal.value);
         default:
             UNREACHABLE;
     }
